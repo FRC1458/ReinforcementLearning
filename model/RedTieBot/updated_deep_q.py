c1da7c6ef9232e6d595753936817c000a5ffc5fc
Neil Ghose <neil@ghose.us>
2020-03-09 15:59:58 -0700

    fixed bugs

--- model/RedTieBot/updated_deep_q.py
+++ model/RedTieBot/updated_deep_q.py
@@ -13,6 +13,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import calculated_path
 import copy
+import time
 from gym import wrappers
 from tensorflow.keras import Model, Sequential
 from tensorflow.keras.layers import Dense, Embedding, Reshape
@@ -47,18 +48,23 @@ class DQN:
     self.env = env
     self.A = calculated_path.Path_Calculator(self.env)
     self.K = K
-    self.D = D
+    self.history = 4
+    self.D = D * self.history
+    self.D_orig = D
     self.is_guided = False
     self.max_x = env.max_x
     self.max_y = env.max_y
     self.max_theta = env.max_facing
     self.num_states = self.max_x * self.max_y * self.max_theta
 
+    self.init_run_cnt = 0
+    self.init_run_max = 0
+    self.obs_space = None
     # create the graph
     
     #self.layers = []
     #self.model = Sequential()
-    inputs = tf.keras.Input(shape = (5,))
+    inputs = tf.keras.Input(shape = (self.D,))
     x = inputs
     #self.model.add(Embedding(D, K, input_length=1))
     M1 = D
@@ -79,38 +85,9 @@ class DQN:
       from_logits=True), optimizer=Adam(0.01))
 
     # collect params for copy
-    '''
-    self.params = []
-    for layer in self.layers:
-      self.params += layer.params
-    
-    # inputs and targets
-    self.X = tf.compat.v1.placeholder(tf.float32, shape=(None, D), name='X')
-    self.G = tf.compat.v1.placeholder(tf.float32, shape=(None,), name='G')
-    self.actions = tf.compat.v1.placeholder(tf.int32, shape=(None,K), name='actions')
-    
-    # calculate output and cost
-    Z = self.X
-    for layer in self.layers:
-      Z = layer.forward(Z)
-    Y_hat = Z
-    self.predict_op = Y_hat
-    selected_action_values = tf.argmax(self.predict_op, output_type = tf.dtypes.float32)
-    
-    selected_action_values = tf.reduce_sum(
-      input_tensor=Y_hat * tf.one_hot(self.actions, K),
-      axis=[1]
-    )
-    
-    
-    cost = tf.reduce_sum(input_tensor=tf.square(self.G - selected_action_values))
-    self.train_op = tf.compat.v1.train.AdamOptimizer(1e-2).minimize(cost)
-    # self.train_op = tf.train.AdagradOptimizer(1e-2).minimize(cost)
-    # self.train_op = tf.train.MomentumOptimizer(1e-3, momentum=0.9).minimize(cost)
-    # self.train_op = tf.train.GradientDescentOptimizer(1e-4).minimize(cost)
-    '''
+   
     # create replay memory
-    self.load()
+    # self.load()
     self.experience = {'s': [], 'a': [], 'r': [], 's2': [], 'done': []}
     self.max_experiences = max_experiences
     self.min_experiences = min_experiences
@@ -123,9 +100,13 @@ class DQN:
     else:
       self.Q = np.random.uniform(low=-1, high=1, size=(self.num_states, self.K))
 
+  def set_max_guided_run(self, n):
+    self.init_run_max = n
     
-  def reset(self):
-    self.is_guided = np.random.random() < 0.25
+  def reset(self, p=.25):
+    self.is_guided = (self.init_run_cnt < self.init_run_max) or np.random.random() < p
+    self.init_run_cnt += 1
+    self.obs_space = None
     self.A.reset()
 
   def set_session(self, session):
@@ -140,61 +121,57 @@ class DQN:
 
   def train(self, target_network):
     # sample a random batch from buffer, do an iteration of GD
+    if self.init_run_cnt < self.init_run_max:
+      self.update_static_reward(target_network)
+      return
     if len(self.experience['s']) < self.min_experiences:
       # don't do anything if we don't have enough experience
       return
     
     # randomly select a batch
-    idx = np.random.choice(len(self.experience['s']), size=self.batch_sz, replace=False)
+    idx = np.random.choice(len(self.experience['s'])-4, size=self.batch_sz, replace=False)
     # print("idx:", idx)
-    states = [self.experience['s'][i] for i in idx]
-    actions = [self.experience['a'][i] for i in idx]
-    rewards = [self.experience['r'][i] for i in idx]
-    next_states = [self.experience['s2'][i] for i in idx]
-    dones = [self.experience['done'][i] for i in idx]
-    '''
-    for i in idx:
-      self.update_reward(self.experience['s'][i], self.experience['a'][i],
-                         self.experience['done'][i], self.experience['r'][i],
-                         self.experience['s2'][i], target_network)
-             
-
-    next_Q = np.max(target_network.predict(next_states), axis=1)
-    targets = [r + self.gamma*next_q if not done else r for r, next_q, done in zip(rewards, next_Q, dones)]
-    '''
+    states = [self.experience['s'][i] + self.experience['s'][i+1] \
+               + self.experience['s'][i+2] + self.experience['s'][i+3] for i in idx]
+    actions = [self.experience['a'][i+3] for i in idx]
+    rewards = [self.experience['r'][i+3] for i in idx]
+    next_states = [self.experience['s2'][i] + self.experience['s2'][i+1] \
+               + self.experience['s2'][i+2] + self.experience['s2'][i+3] for i in idx]
+    dones = [self.experience['done'][i+3] for i in idx]
     
-    targets = [self.update_reward(self.experience['s'][i], self.experience['a'][i],
-                         self.experience['done'][i], self.experience['r'][i],
-                         self.experience['s2'][i], target_network) for i in idx ]
+    targets = [self.update_reward(self.experience['s'][i+3], self.experience['a'][i+3],
+                         self.experience['done'][i+3], self.experience['r'][i+3],
+                         self.experience['s2'][i] + self.experience['s2'][i+1] \
+               + self.experience['s2'][i+2] + self.experience['s2'][i+3],
+                                  target_network) for i in idx ]
     # call optimizer
-    '''
-    self.session.run(
-      self.train_op,
-      feed_dict={
-        self.X: states,
-        self.G: targets,
-        self.actions: actions
-      }
-    )
-    '''
+    
     self.model.fit([states], [targets], verbose = 0)
 
+  def update_static_reward(self, target):
+    i = -1
+    state, action, is_done, reward, next_state = \
+           self.experience['s'][i], self.experience['a'][i], \
+                         self.experience['done'][i], self.experience['r'][i], \
+                         self.experience['s2'][i]
+    target.Q[self.q_state_idx(state[0], state[1], state[2])][action] = reward
+    
+    
   def update_reward(self, state, action, is_done, reward, next_state, target):
     if is_done:
-      target.Q[state[2]+self.max_theta*state[1] + \
-             self.max_y*self.max_theta*state[0]][action] = reward
+      target.Q[self.q_state_idx(state[0], state[1], state[2])][action] = reward
       return action
     else:
       a = target.predict(next_state)
-      vi = np.argmax(target.Q[next_state[2]+self.max_theta*next_state[1] + \
-             self.max_y*self.max_theta*next_state[0]])
-      target.Q[state[2]+self.max_theta*state[1] + \
-             self.max_y*self.max_theta*state[0]][action] = reward + self.gamma * \
-             target.Q[next_state[2]+self.max_theta*next_state[1] + \
-             self.max_y*self.max_theta*next_state[0]][vi]
+      vi = np.argmax(target.Q[self.q_state_idx(next_state[-5], next_state[-4], next_state[-3])])
+      target.Q[self.q_state_idx(state[0], state[1], state[2])][
+        action] = reward + self.gamma * \
+             target.Q[self.q_state_idx(next_state[0], next_state[1], next_state[2])][vi]
       return a
-               
 
+  def q_state_idx(self, x, y, theta):
+    return theta + self.max_theta * y + self.max_y * self.max_theta * x
+  
   def add_experience(self, s, a, r, s2, done):
     if len(self.experience['s']) >= self.max_experiences:
       self.experience['s'].pop(0)
@@ -208,6 +185,16 @@ class DQN:
     self.experience['s2'].append(s2)
     self.experience['done'].append(done)
 
+  def obs_x(self, x):
+    if self.obs_space is None:
+      self.obs_space =  list(x.values())+  list(x.values())+\
+                       list(x.values())+ list(x.values())
+    else:
+      for i in range(self.D_orig):
+        self.obs_space.pop(0)
+      self.obs_space += list(x.values())
+    return self.obs_space
+      
   def sample_action(self, x, eps):
     if self.is_guided:
       return self.A.calculated_path(x)
@@ -215,7 +202,7 @@ class DQN:
       if np.random.random() < eps:
         return self.env.action_space.sample()
       else:
-        p=self.predict(list(x.values()))
+        p=self.predict(self.obs_x(x))
         return self.env.action_space.fromQ(np.argmax(p))
 
 
@@ -224,8 +211,8 @@ def play_one(env, model, tmodel, eps, gamma, copy_period):
   done = False
   totalreward = 0
   iters = 0
-  model.reset()
-  tmodel.reset()
+  model.reset(.2)
+  # tmodel.reset()
   
   while not done and iters < 10000:
     action = model.sample_action(observation, eps)
@@ -250,12 +237,13 @@ def main():
   env = gym.make('redtiebot-v0')
   gamma = 0.99
   copy_period = 50
-
+  s_time = time.time()
   D = 5
   K = 9
   sizes = [10,15,20,15,10]
   model = DQN(D, K, sizes, gamma, env)
   tmodel = DQN(D, K, sizes, gamma, env)
+  tmodel.load()
   init = tf.compat.v1.global_variables_initializer()
   #session = tf.compat.v1.InteractiveSession()
   #session.run(init)
@@ -270,6 +258,7 @@ def main():
 
 
   N = 500
+  model.set_max_guided_run(int(.25*N))
   totalrewards = np.empty(N)
   costs = np.empty(N)
   for n in range(N):
@@ -278,6 +267,8 @@ def main():
     totalrewards[n] = totalreward
     if n % 100 == 0:
       print("episode:", n, "total reward:", totalreward, "eps:", eps, "avg reward (last 100):", totalrewards[max(0, n-100):(n+1)].mean())
+      print("time: "+str(time.time()-s_time))
+      s_time = time.time()
 
   print("avg reward for last 100 episodes:", totalrewards[-100:].mean())
   print("total steps:", totalrewards.sum())
